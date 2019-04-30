24f800503ac533b9e0522aa659474f1ef49ed76e
klitscher <672@holbertonschool.com>
2019-04-30 18:00:52 +0000

    Update so it matches correct output

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,11 +5,11 @@ if number >= 0:
     modNum = number % 10
 else:
     modNum = number % -10
-str = "last digit of"
+str = "Last digit of"
 str2 = "is less than 6 and not 0"
 if modNum > 5:
     print("{} {:d} is {:d} and is greater than 5".format(str, number, modNum))
 elif modNum == 0:
     print("{} {:d} is {:d} and is 0".format(str, number, modNum))
 else:
-    print("{} {:d} is {:-d} and {}".format(str, number, modNum, str2))
+    print("{} {:d} is {:d} and {}".format(str, number, modNum, str2))
