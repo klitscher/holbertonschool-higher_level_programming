17f8806f4eaba8416ef83f03fbe04bac98837bec
klitscher <672@holbertonschool.com>
2019-04-30 17:47:02 +0000

    Adding exercise to check number

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+modNum = number % 10
+str = "last digit of"
+str2 = "is less than 6 and not 0"
+if modNum > 5:
+    print("{} {:d} is {:d} and is greater than 5".format(str, number, modNum))
+elif modNum == 0:
+    print("{} {:d} is {:d} and is 0".format(str, number, modNum))
+else:
+    print("{} {:d} is {:d} and {}".format(str, number, modNum, str2))
