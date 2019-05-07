5e3ee2047b6422bda19bee0a37a9b4521b79edf0
klitscher <672@holbertonschool.com>
2019-05-07 23:51:14 +0000

    Adding files for exercise 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: linked list to check
+ *
+ * Return: 0 if not palindrome, 1 if it is
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *current;
+	int *arr;
+	int end = 0;
+	int beg = 0;
+
+	if (head == NULL)
+		return (0);
+	if (*head == NULL)
+		return (1);
+	current = *head;
+	while (current != NULL)
+	{
+		current = current->next;
+		end++;
+	}
+	arr = malloc(sizeof(int) * end);
+	if (arr == NULL)
+		return (0);
+	current = *head;
+	end = 0;
+	while (current != NULL)
+	{
+		arr[end] = current->n;
+		current = current->next;
+		end++;
+	}
+	end--;
+	for (; beg < end; beg++, end--)
+	{
+		if (arr[beg] != arr[end])
+		{
+			free(arr);
+			return (0);
+		}
+	}
+	free(arr);
+	return (1);
+}
