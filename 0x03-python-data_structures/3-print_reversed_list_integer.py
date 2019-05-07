2ab30f0574f89ed64e65330f9ac36abc5e982c6f
klitscher <672@holbertonschool.com>
2019-05-07 17:10:59 +0000

    Add condition for if we are passed None

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list):
+def print_reversed_list_integer(my_list=[]):
+    if my_list == None:
+        return
     list.reverse(my_list)
     for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
