7248216066bd4c313eb7de922edca53adfc9a68d
klitscher <672@holbertonschool.com>
2019-05-09 23:11:33 +0000

    Updated for the list is empty

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return None
-    for a, x in a_dictionary.items():
-        if x is None:
-            return None
-    high = max([val for key, val in a_dictionary.items()])
-    highScore = [key for key, val in a_dictionary.items() if val == high]
-    highScore = ", ".join(highScore)
-    return highScore
+    if a_dictionary is not None and len(a_dictionary) > 0:
+        high = max([val for key, val in a_dictionary.items()])
+        highScore = [key for key, val in a_dictionary.items() if val == high]
+        highScore = ", ".join(highScore)
+        return highScore
+    return None
