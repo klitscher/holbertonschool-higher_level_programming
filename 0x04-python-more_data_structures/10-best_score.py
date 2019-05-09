5fcf83ff625e739cd49647f6d71c6a9f13655a00
klitscher <672@holbertonschool.com>
2019-05-09 22:52:11 +0000

    Updating for if they pass a list with a score of none

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
+    for a, x in a_dictionary.items():
+        if x is None:
+            return None
     high = max([val for key, val in a_dictionary.items()])
     highScore = [key for key, val in a_dictionary.items() if val == high]
     highScore = ", ".join(highScore)
