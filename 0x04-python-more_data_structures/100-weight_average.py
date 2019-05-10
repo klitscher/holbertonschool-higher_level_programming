4cd7c73cbdc81059ef62592c5b934618ead11b45
klitscher <672@holbertonschool.com>
2019-05-10 02:23:12 +0000

    Updating for if list is empty

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if my_list is None or len(my_list) < 1:
+        return 0
     div = sum(j for i, j in my_list)
     product = (a * b for a, b in my_list)
     sum1 = 0
