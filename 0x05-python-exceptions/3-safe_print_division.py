8eb1b5bd440ae7b3164d62bbf00a9210cfa35c38
klitscher <672@holbertonschool.com>
2019-05-20 16:36:53 +0000

    Update code for exercise 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    quotient = 0
+    try:
+        quotient = a / b
+        return quotient
+    except ZeroDivisionError:
+        return None
+    finally:
+        if b == 0:
+            print("Inside result: {}".format(None))
+        else:
+            print("Inside result: {:.1f}".format(quotient))
