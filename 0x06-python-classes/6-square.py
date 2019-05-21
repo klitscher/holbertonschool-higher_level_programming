12493dbd183931dd83a661242d08711cff56a4be
klitscher <672@holbertonschool.com>
2019-05-21 22:20:45 +0000

    Update to move the box down by one

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -8,6 +8,11 @@ class Square:
         size (int): size of the square.
         position: Coordinate of the square
         """
+
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
         self.__size = size
         if not isinstance(position, tuple):
             raise TypeError('position must be a tuple of 2 positive integers')
