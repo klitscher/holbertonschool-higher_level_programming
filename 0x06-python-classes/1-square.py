c6a2801e40ac2d50e98b7a94c8bad6dbfcae416c
klitscher <672@holbertonschool.com>
2019-05-21 18:17:07 +0000

    Update file for exercise 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """Class to define a square."""
+    def __init__(self, size):
+        """Initialize class with size.
+
+        Args:
+        size (int): size of the square.
+        """
+        self.__size = size
