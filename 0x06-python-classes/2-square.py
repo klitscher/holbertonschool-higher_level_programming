4d340f52683eb3c24ceeabb8c1a5bd7c8b53b751
klitscher <672@holbertonschool.com>
2019-05-21 19:04:11 +0000

    Update file for exercise 3

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+class Square:
+    """Class to define a square."""
+    def __init__(self, size=0):
+        """Initialize class with size.
+
+        Check to see if the size entered is not an integer
+
+        Args:
+        size (int): size of the square.
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
