e0a36d51154c81784d183c861bc4b10ed6a0634c
klitscher <672@holbertonschool.com>
2019-05-21 19:38:08 +0000

    Update file for exercise 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,1 +1,39 @@
 #!/usr/bin/python3
+class Square:
+    """Class to define a square."""
+    def __init__(self, size=0):
+        """Initialize class with size.
+        Check to see if the size entered is not an integer
+        Args:
+        size (int): size of the square.
+        """
+        self.__size = size
+
+    def area(self):
+        """Return the area of a square"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """int: Returns the size of the square.
+        Checks for errors, otherwise assigne value to the field__size.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def my_print(self):
+        """Prints a visual representation of the square"""
+        if self.__size == 0:
+            print()
+        else:
+            for row in range(0, self.__size):
+                for col in range(0, self.__size):
+                    print("#".format(), end="")
+                print()
