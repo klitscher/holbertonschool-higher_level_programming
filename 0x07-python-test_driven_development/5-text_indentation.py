078eced624a9ab4659957d21232440661a166ffb
klitscher <672@holbertonschool.com>
2019-05-24 19:18:28 +0000

    Update to remove duplicate code

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -12,18 +12,11 @@ def text_indentation(text):
     count = 0
     new = ""
     for i in range(len(text)):
-        if text[i] == ".":
-            new = text[count:(i + 1)]
-            print("{}\n".format(new.lstrip()))
-            count = i + 1
-        elif text[i] == "?":
-            new = text[count:(i + 1)]
-            print("{}\n".format(new.lstrip()))
-            count = i + 1
-        elif text[i] == ":":
+        if text[i] in '.?:':
             new = text[count:(i + 1)]
             print("{}\n".format(new.lstrip()))
             count = i + 1
         elif i + 1 == len(text):
             new = text[count:(i + 1)]
             print("{}".format(new.lstrip()), end="")
+
