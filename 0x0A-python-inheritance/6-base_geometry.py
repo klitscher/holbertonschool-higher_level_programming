023718a105c5037372d2584947a2d5e1cfb0a996
klitscher <672@holbertonschool.com>
2019-06-05 19:11:52 +0000

    Update file for exercise 6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Module to create a BaseGeometry class"""
+
+
+class BaseGeometry:
+    """Base Class"""
+    def area(self):
+        """Get area of the geometry"""
+        raise Exception('area() is not implemented')
