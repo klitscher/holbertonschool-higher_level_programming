a691b575caa0e37ec650e496f773944fc335d204
klitscher <672@holbertonschool.com>
2019-06-05 16:37:40 +0000

    Udpating with documentation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""Module to return an object's attributes and methods"""
+
+
 def lookup(obj):
+    """Function to return an object's attributes and methods"""
     return dir(obj)
