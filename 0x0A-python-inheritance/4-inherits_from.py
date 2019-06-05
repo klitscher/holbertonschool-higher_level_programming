839825a5b23fd624f285ad2eec5e267d6544d914
klitscher <672@holbertonschool.com>
2019-06-05 19:05:52 +0000

    Updating for exercise 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+"""Checks if is subclass"""
+
+
 def inherits_from(obj, a_class):
+    """Checks if is subclass"""
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return True
+    else:
+        return False
