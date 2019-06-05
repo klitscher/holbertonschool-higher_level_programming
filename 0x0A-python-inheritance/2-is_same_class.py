922e9d560adc0ed979019355c51f6d44cd2cb592
klitscher <672@holbertonschool.com>
2019-06-05 18:31:05 +0000

    Update file for exercise 2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+"""Module to return if object is exactly an instance"""
+
+
 def is_same_class(obj, a_class):
+    """Function to check object"""
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
