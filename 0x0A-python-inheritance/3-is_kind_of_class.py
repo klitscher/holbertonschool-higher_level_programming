ae81adf7de96ca77f661cc6d3816ab6e9491bdd8
klitscher <672@holbertonschool.com>
2019-06-05 18:35:19 +0000

    Update for exercise 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+"""Checks the inheritence of an object"""
+
 def is_kind_of_class(obj, a_class):
+    """Checks the inheritence of an object"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
