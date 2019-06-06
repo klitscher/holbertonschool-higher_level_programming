ce6f97c5bb23aa25fe687ff7cb1d2b4a5cf5e3a4
klitscher <672@holbertonschool.com>
2019-06-06 18:51:13 +0000

    Update file for exercise 6

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+"""Module to return a python object from JSON"""
+
+import json
+
+
 def from_json_string(my_str):
+    """Function to return a python object from JSON"""
+
+    return json.loads(my_str)
