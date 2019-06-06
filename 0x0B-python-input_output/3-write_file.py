ca603ab01b1e0a35d28caf1b7aff7cc11794facd
klitscher <672@holbertonschool.com>
2019-06-06 18:35:57 +0000

    Update file for exercise 3

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+"""Module to write to a file"""
+
+
 def write_file(filename="", text=""):
+    """Function to write to a file"""
+
+    with open(filename, encoding="utf-8", mode='w') as myFile:
+        myFile.write(text)
+    return(len(text))
