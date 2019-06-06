6634f1cfa9fa082796b3c1f96ca0cc6e80366b0c
klitscher <672@holbertonschool.com>
2019-06-06 22:38:29 +0000

    Update file for exercise 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""Method to create a student"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieve a dictionary representation of student"""
+        li = self.__dict__
+        dic = {}
+        for key, value in li.items():
+            if isinstance(value, list) or isinstance(
+                    value, dict) or isinstance(
+                        value, str) or isinstance(
+                            value, int) or isinstance(value, bool):
+                dic.update({key: value})
+        return dic
