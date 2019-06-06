e3e8c1e55b22317bf12e809022b111c244844278
klitscher <672@holbertonschool.com>
2019-06-06 21:02:53 +0000

    Adding file for exercise 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
-def class_to_json(obj):
+"""Module to add arguments to python list"""
+import sys
+import os.path
+
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+
+if os.path.isfile("./add_item.json"):
+    jstring = load_from_json_file("add_item.json")
+    li = sys.argv[1:len(sys.argv)]
+    fullstr = jstring + li
+    save_to_json_file(fullstr, "add_item.json")
+else:
+    li = sys.argv[1:len(sys.argv)]
+    save_to_json_file(li, "add_item.json")
