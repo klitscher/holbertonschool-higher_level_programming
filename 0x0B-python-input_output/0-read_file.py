fad97e28908df3c31fe0176a2547bf8e811ca7f3
klitscher <672@holbertonschool.com>
2019-06-06 18:01:42 +0000

    Updating file for exercise 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,4 +4,7 @@
 
 def read_file(filename=""):
     """Function to read a file"""
-    
+
+    with open(filename, encoding="utf-8") as myFile:
+        print(myFile.read(), end="")
+    myFile.close()
