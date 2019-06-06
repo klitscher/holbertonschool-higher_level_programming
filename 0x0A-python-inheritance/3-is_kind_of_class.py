e86632d9d7744ef5e336d7b3cec72ac871217981
klitscher <672@holbertonschool.com>
2019-06-06 16:47:57 +0000

    Update for style

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Checks the inheritence of an object"""
 
+
 def is_kind_of_class(obj, a_class):
     """Checks the inheritence of an object"""
     if isinstance(obj, a_class):
