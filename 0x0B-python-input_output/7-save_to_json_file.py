48dcbbaf902d6b3bcbbdf19948bbfeec2d30a85f
klitscher <672@holbertonschool.com>
2019-06-06 19:00:41 +0000

    Update file for exercise 7

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+"""Module to write an object ot a text file using JSON"""
+
+
+import json
+
+
 def save_to_json_file(my_obj, filename):
+    """Function to write an object ot a text file using JSON"""
+
+    with open(filename, encoding="utf-8", mode='w') as myFile:
+        myFile.write(json.dumps(my_obj))
