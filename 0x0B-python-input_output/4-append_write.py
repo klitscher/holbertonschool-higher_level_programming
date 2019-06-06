aa6ce7558cc900952e597832ab8d63a0517bcc7a
klitscher <672@holbertonschool.com>
2019-06-06 18:41:43 +0000

    Update file for exercise 4

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+"""Module to append text to a file"""
+
+
 def append_write(filename="", text=""):
+    """Function to append text to a file"""
+
+    with open(filename, encoding="utf-8", mode="a") as myFile:
+        myFile.write(text)
+    return len(text)
