a922ee6e890e100bd358c2f1d88048a8f80e792d
klitscher <672@holbertonschool.com>
2019-06-06 18:28:48 +0000

    Updating file for exercise 2

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
+"""Module to read number of lines"""
+
+
 def read_lines(filename="", nb_lines=0):
+    """Function to read number of lines"""
+
+    with open(filename, encoding="utf-8") as myFile:
+        if nb_lines <= 0:
+            print(myFile.read(), end="")
+            return
+        for i in range(nb_lines):
+            print(myFile.readline(), end="")
