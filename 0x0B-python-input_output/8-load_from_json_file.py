c6cf426fddfc4806146d8638c2f3ac9e7b68bfa7
klitscher <672@holbertonschool.com>
2019-06-06 19:26:21 +0000

    Update file for exercise 8

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+"""Module to create an object form a json file"""
+
+
+import json
+
+
 def load_from_json_file(filename):
+    """Function to create an object form a json file"""
+
+    with open(filename, encoding="utf-8") as myFile:
+        return json.load(myFile)
