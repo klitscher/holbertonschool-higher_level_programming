1c6e9ac7879f94aee208f4c9a8834fe43e053739
klitscher <672@holbertonschool.com>
2019-06-07 01:35:13 +0000

    Update file for exercise 14

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Method for a pascal function"""
+
+
+def pascal_triangle(n):
+    """Returns a list of lists of integers representing
+    the pascals triangle of n
+    """
+    actual = []
+    li = [1]
+    if n <= 0:
+        return actual
+    for row in range(n):
+        actual.append(li)
+        new = []
+        new.append(li[0])
+        for col in range(len(li) - 1):
+            new.append(li[col] + li[col + 1])
+        new.append(li[-1])
+        li = new
+    return actual
