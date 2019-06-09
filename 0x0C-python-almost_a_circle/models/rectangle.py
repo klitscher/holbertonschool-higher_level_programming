5f4a7294c5f6569f75abf2fbed6bac4d02b57511
klitscher <672@holbertonschool.com>
2019-06-09 16:13:51 -0400

    Updating file with basic rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Module for rectangle class"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Class rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Instantiation"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @propety
+    def width(self):
+        """Getter for width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for width"""
+        self.__width = value
+
+    @propety
+    def height(self):
+        """Getter for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for width"""
+        self.__height = height
+
+    @property
+    def x(self):
+        """Getter for x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter for x"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter for y"""
+        return self.__y
+
+    @x.setter
+    def y(self, value):
+        """Setter for y"""
+        self.__y = value
