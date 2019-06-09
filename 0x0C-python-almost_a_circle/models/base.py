d2cd8767890a300afcd2a10c47e37b66adeb659c
klitscher <672@holbertonschool.com>
2019-06-09 13:55:26 -0400

    Adding test folder and updating base file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Module for creating a base class"""
+
+
+class Base:
+    """Basic class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Instantiation"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Module to test Base class"""
+
+
+import unittest
+from models.base import Base
+
+class TestBaseClass(unittest.TestCase):
+    """Tests for Base class"""
+
+    def test_noArgs(self):
+        """Test no argumetns"""
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+    def test_multiArgs(self):
+        """Test multiple arguments"""
+        self.assertRaises(TypeError, Base, 'Hello', 1)
+
+    def test_intArgs(self):
+        """Test int args"""
+        b1 = Base(5)
+        self.assertEqual(b1.id, 5)
+
+    def test_floatArgs(self):
+        """Test float args"""
+        b1 = Base(4.78)
+        self.assertEqual(b1.id, 4.78)
+
+    def test_expressionArg(self):
+        """Test expression argument"""
+        b1 = Base(3 * 8)
+        self.assertEqual(b1.id, 24)
+
+    def test_strArg(self):
+        """Test string argument"""
+        b1 = Base('Hello')
+        self.assertEqual(b1.id, 'Hello')
+
+    def test_listArg(self):
+        """Test list as argument"""
+        b1 = Base([1, 2, 3])
+        self.assertEqual(b1.id, [1, 2, 3])
+
+    def test_tupleArgs(self):
+        """Test tuples as arguments"""
+        b1 = Base((1, 2, 3))
+        self.assertEqual(b1.id, (1, 2, 3))
+
+    def test_dictArgs(self):
+        """Test dict as arguments"""
+        b1 = Base({"Key": 'value', "key2": 3})
+        self.assertEqual(b1.id, {"Key": 'value', "key2": 3})
