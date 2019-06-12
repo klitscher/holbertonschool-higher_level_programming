41e129d3b86b580bf221aed38c8323a76e4e7cc1
klitscher <672@holbertonschool.com>
2019-06-12 17:04:43 +0000

    Update in order to use function

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,9 +32,10 @@ class Base:
 
         lis = []
         if list_objs is None:
+            js_dic = cls.to_json_string(lis)
             with open(cls.__name__ + ".json", encoding="utf-8",
                       mode="a+") as myFile:
-                myFile.write(lis)
+                myFile.write(js_dic)
         else:
             for li in list_objs:
                 lis.append(li.to_dictionary())
