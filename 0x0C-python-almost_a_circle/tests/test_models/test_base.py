1aceb5750d9475b6a629c5b77787dd6fa9a82dc6
klitscher <672@holbertonschool.com>
2019-06-12 21:42:52 +0000

    Update tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -107,7 +107,17 @@ class TestBaseClass(unittest.TestCase):
 
     #----------------Tests save_to_file----------------------------
 
-    #Need Tests for this
+    def test_saveToFileTooManyArgs(self):
+        """Test too many args"""
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        self.assertRaises(TypeError, Rectangle.save_to_file, 1, 1)
+
+        def test_saveToFileTooManyArgs(self):
+        """Test too many args"""
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        self.assertRaises(TypeError, Rectangle.save_to_file, 1)
 
     #----------------Tests from_json_string----------------------------
 
