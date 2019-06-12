d091f4c27cedaccfaab3d3147b12afd4fefb0943
klitscher <672@holbertonschool.com>
2019-06-12 17:21:11 +0000

    Update to actually follow directions

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -34,14 +34,14 @@ class Base:
         if list_objs is None:
             js_dic = cls.to_json_string(lis)
             with open(cls.__name__ + ".json", encoding="utf-8",
-                      mode="a+") as myFile:
+                      mode="w") as myFile:
                 myFile.write(js_dic)
         else:
             for li in list_objs:
                 lis.append(li.to_dictionary())
             js_dic = cls.to_json_string(lis)
             with open(cls.__name__ + ".json", encoding="utf-8",
-                      mode="a+") as myFile:
+                      mode="w") as myFile:
                 myFile.write(js_dic)
 
     @staticmethod
