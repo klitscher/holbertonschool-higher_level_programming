89a0a35155c164663aefe5113913bee0880508d3
klitscher <672@holbertonschool.com>
2019-07-15 20:52:58 +0000

    Updating file for exercise 15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- List number of records
 -- Have to have same score
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY number DESC HAVING number >= 1;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score DESC HAVING number >= 1;
