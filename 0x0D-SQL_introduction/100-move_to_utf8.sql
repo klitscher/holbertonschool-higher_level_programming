80812be1846f65492d9cdadfb33ac716bd2d5bd1
klitscher <672@holbertonschool.com>
2019-07-15 23:03:59 +0000

    Adding line to actually go into database

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,5 @@
 -- Converts to a UTF8 Database
--- Breaks
+-- Script that goes into database and chnages all the characters
+USE hbtn_0c_0
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
