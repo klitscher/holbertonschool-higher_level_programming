5726289c89725874078b21a0b5c92f50c0cc9cff
klitscher <672@holbertonschool.com>
2019-07-15 23:58:34 +0000

    Updating file for exercise 101

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,3 @@
+-- Create script that displays average temp
+-- See above
+SELECT MAX(city), avg_temp FROM (SELECT DISTINCT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city) AS T2 GROUP BY avg_temp DESC;
