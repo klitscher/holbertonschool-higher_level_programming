07d3afc7ed3e22b2dda05a9f4aa35ba747b290eb
klitscher <672@holbertonschool.com>
2019-07-16 01:34:03 +0000

    Removing redundent sub

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,9 +1,3 @@
 -- Getting more temperature data
 -- Average data for 2 months
-SELECT MAX(test) as city, avg_temp FROM (
-       SELECT DISTINCT MAX(city) AS test, ROUND(AVG(avg_temp), 4) AS avg_temp FROM (
-       	      SELECT DISTINCT city, month, ROUND(AVG(value), 4) as AVG_temp
-       	      FROM temperatures WHERE `month` = 8 or `month` = 7
-	      GROUP BY `city`, month)
-	      AS t2 GROUP BY `city`)
-AS t3 GROUP BY avg_temp DESC LIMIT 3;
+SELECT city, avg_temp FROM (SELECT DISTINCT city, AVG(value) as avg_temp FROM temperatures WHERE `month` = 8 or `month` = 7 GROUP BY `city`) AS t2 GROUP BY avg_temp DESC, city LIMIT 3;
