c65e01cb2a7b319be624b93a4a4612cfefc10730
klitscher <672@holbertonschool.com>
2019-07-16 12:07:42 -0400

    Update to grant privelegers

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,6 @@
 -- Script to create a new user
 -- See above
 CREATE USER IF NOT EXISTS 'user_0d_1'@localhost IDENTIFIED BY 'user_0d_1_pwd';
+
+--Grants priveleges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
