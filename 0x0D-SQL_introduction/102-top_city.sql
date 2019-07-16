a99d4099b80c22f914a64188f518e31f05fee4f2
klitscher <672@holbertonschool.com>
2019-07-16 01:35:09 +0000

    Making more readable

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,6 @@
 -- Getting more temperature data
 -- Average data for 2 months
-SELECT city, avg_temp FROM (SELECT DISTINCT city, AVG(value) as avg_temp FROM temperatures WHERE `month` = 8 or `month` = 7 GROUP BY `city`) AS t2 GROUP BY avg_temp DESC, city LIMIT 3;
+SELECT city, avg_temp FROM (
+       SELECT DISTINCT city, AVG(value) as avg_temp
+       FROM temperatures WHERE `month` = 8 or `month` = 7 GROUP BY `city`)
+AS t2 GROUP BY avg_temp DESC, city LIMIT 3;
