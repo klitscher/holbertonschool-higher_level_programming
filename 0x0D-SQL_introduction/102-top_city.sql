7a0bd313dc3f84848cf97611281d2065ee2740f5
klitscher <672@holbertonschool.com>
2019-07-16 00:45:32 +0000

    Removing 0's and round to the 4th place

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,8 +1,8 @@
 -- Getting more temperature data
 -- Average data for 2 months
 SELECT MAX(city) as city, avg_temp FROM (
-       SELECT DISTINCT MAX(city) AS city, AVG(avg_temp) AS avg_temp FROM (
-       	      SELECT DISTINCT city, month, AVG(value) as AVG_temp
+       SELECT DISTINCT MAX(city) AS city, ROUND(AVG(avg_temp), 4) AS avg_temp FROM (
+       	      SELECT DISTINCT city, month, ROUND(AVG(value), 4) as AVG_temp
        	      FROM temperatures WHERE `month` = 8 or `month` = 7
 	      GROUP BY city, month)
 	      AS t2 GROUP BY city)
