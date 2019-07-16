4bdd2b34531c6a4d42705bb33d5a2a871344d080
klitscher <672@holbertonschool.com>
2019-07-16 12:12:19 -0400

    Fixing comment

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,5 +2,5 @@
 -- See above
 CREATE USER IF NOT EXISTS 'user_0d_1'@localhost IDENTIFIED BY 'user_0d_1_pwd';
 
---Grants priveleges
+-- Grants privileges
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
