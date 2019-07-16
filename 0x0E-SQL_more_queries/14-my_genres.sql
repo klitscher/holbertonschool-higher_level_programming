983cbe86d6108a6142756c004d8a80cf7c6dd177
klitscher <672@holbertonschool.com>
2019-07-16 18:38:49 +0000

    Updating file for exercise 14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,5 @@
+-- List all generes on a specific show
+-- See above
+SELECT tv_genres.name FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_show_genres.show_id = 8 ORDER BY tv_genres.name ASC;
