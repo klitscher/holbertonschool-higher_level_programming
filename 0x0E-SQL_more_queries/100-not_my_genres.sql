0065a09dadc775f5e2c36cb4d0bd9386100a1b17
klitscher <672@holbertonschool.com>
2019-07-16 21:18:54 +0000

    Update to order correctly

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -6,4 +6,5 @@ LEFT JOIN (
      INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
      INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
      WHERE tv_shows.title = 'Dexter' ORDER BY tv_genres.name)
-AS tb2 ON tv_genres.name = bad WHERE bad IS NULL;
+AS tb2 ON tv_genres.name = bad WHERE bad IS NULL
+ORDER BY tv_genres.name ASC;
