d3ebb67547e38e98b81bd57b52479b214e04c46f
klitscher <672@holbertonschool.com>
2019-07-16 21:17:19 +0000

    Update file for exercise 100

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,9 @@
+-- Script to list all genres not linked to a value
+-- See above
+SELECT tv_genres.name FROM tv_genres
+LEFT JOIN (
+     SELECT tv_genres.name AS bad FROM tv_genres
+     INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+     INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+     WHERE tv_shows.title = 'Dexter' ORDER BY tv_genres.name)
+AS tb2 ON tv_genres.name = bad WHERE bad IS NULL;
