eb6d54d4e3b59874c71c0a117441c03147a5087a
klitscher <672@holbertonschool.com>
2019-08-09 21:45:09 +0000

    Updating file for exercise 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""Script to filter states by user"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    username = argv[1]
+    passwd = argv[2]
+    dbname = argv[3]
+    sname = argv[4]
+
+    database = MySQLdb.connect(host="localhost",
+                               port=3306,
+                               user=username,
+                               passwd=passwd,
+                               db=dbname)
+
+    cur = database.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name ='{}' ORDER BY id ASC".format(sname))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    database.close()
