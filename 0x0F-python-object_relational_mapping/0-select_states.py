2b1b1b85a87a47f5fdc81df705c93ae2bae36126
klitscher <672@holbertonschool.com>
2019-08-09 21:01:10 +0000

    Updating for pep8 compliance

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,23 +2,22 @@
 """Script to lists all states form a database"""
 
 
-import MySQLdb
-from sys import argv
-
-
 if __name__ == "__main__":
-        username = argv[1]
-        passwd = argv[2]
-        dbname = argv[3]
+    import MySQLdb
+    from sys import argv
+    username = argv[1]
+    passwd = argv[2]
+    dbname = argv[3]
 
-        database = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=passwd, db=dbname)
-        cur = database.cursor()
-
-        cur.execute("SELECT * FROM states ORDER BY id ASC")
-        rows = cur.fetchall()
-
-        for row in rows:
-            print("{}".format(row))
-
-        cur.close()
-        database.close()
+    database = MySQLdb.connect(host="localhost",
+                               port=3306,
+                               user=username,
+                               passwd=passwd,
+                               db=dbname)
+    cur = database.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print("{}".format(row))
+    cur.close()
+    database.close()
