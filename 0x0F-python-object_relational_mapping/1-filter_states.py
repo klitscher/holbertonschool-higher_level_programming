49fa14ae63a8a382bde14a701a6896d2d2a2313a
klitscher <672@holbertonschool.com>
2019-08-09 21:23:05 +0000

    Updating 1 for exercise 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""Script to filter states"""
+
+
+if __name__ == '__main__':
+    from sys import argv
+    import MySQLdb
+
+    username = argv[1]
+    password = argv[2]
+    dbname = argv[3]
+
+    database = MySQLdb.connect(host="localhost",
+                               port=3306,
+                               user=username,
+                               passwd=password,
+                               db=dbname)
+    cur = database.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    database.close()
