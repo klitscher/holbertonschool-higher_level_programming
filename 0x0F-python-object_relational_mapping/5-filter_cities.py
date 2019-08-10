e27923f611c18371939f9cdc51f923112e904a67
klitscher <672@holbertonschool.com>
2019-08-10 00:01:22 +0000

    Updating for exercise 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""Lists all cities from a databases that match argument"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    username = argv[1]
+    passwd = argv[2]
+    dbname = argv[3]
+    sname = argv[4]
+    database = MySQLdb.connect(host="localhost",
+                               port=3306,
+                               user=username,
+                               passwd=passwd,
+                               db=dbname)
+
+    cur = database.cursor()
+    cur.execute(
+        "SELECT cities.name FROM cities \
+        INNER JOIN states ON states.id = cities.state_id \
+        WHERE states.name = %s;", (sname,))
+    rows = cur.fetchall()
+    s = []
+    for row in rows:
+        s.append(row[0])
+    print(', '.join(s))
+    cur.close()
+    database.close()
