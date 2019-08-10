f6c7a5ccfab0ad9af2a4c052928d8ec452bf48bd
klitscher <672@holbertonschool.com>
2019-08-10 22:12:55 +0000

    Adding file for exercise 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""Prints state that matches name passed"""
+
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(State.name == argv[4]).one_or_none()
+
+    if states is not None:
+        print(states.id)
+    else:
+        print("Not found")
+
+    session.close()
