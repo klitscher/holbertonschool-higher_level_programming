684228076163fa7b648999c507ecb9e15726bd2f
klitscher <672@holbertonschool.com>
2019-08-10 23:17:43 +0000

    Updating file for exercise 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""Module to list all states that contain an a"""
+
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(State.id.contains('2')).first()
+    states.name = 'New Mexico'
+
+    session.flush()
+    session.commit()
+
+    session.close()
