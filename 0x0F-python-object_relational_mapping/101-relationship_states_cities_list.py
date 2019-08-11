b28b30d4d37aa0eaf9b4f1f1aa4bc866c4b1f413
klitscher <672@holbertonschool.com>
2019-08-11 18:03:20 +0000

    Updating file for exercise 16

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""Module to list all states that contain an a"""
+
+
+from sys import argv
+from relationship_city import Base, City
+from relationship_state import State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import joinedload
+
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).options(joinedload(State.cities))
+
+    for states in state:
+        print("{}: {}".format(states.id, states.name))
+        for i in range(len(states.cities)):
+            print("\t{}: {}".format(
+                states.cities[i].id, states.cities[i].name))
+
+    session.close()
