3f6607eb94729d62c96784b6e07da1a5d1874fd1
klitscher <672@holbertonschool.com>
2019-08-20 15:53:07 -0400

    Updating answer to be the best worst case log(n) instead of n

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,16 +3,23 @@
 
 
 def find_peak(list_of_integers):
-    """Function to find peak"""
-
-    if len(list_of_integers) < 1:
+    """Driver Function for peak"""
+    length = len(list_of_integers)
+    if length < 1:
         return ("None")
-    if len(list_of_integers) == 1:
-        return (list_of_integers[0])
-    base = list_of_integers[0]
-    for i in range(1, len(list_of_integers)):
-        if base > list_of_integers[i]:
-            return (base)
-        if base < list_of_integers[i]:
-            base = list_of_integers[i]
-    return (base)
+    result = get_peak(list_of_integers, 0, length - 1, length)
+    return (result)
+
+def get_peak(int_list, low, high, n):
+    """Work function for peak"""
+    mid = int(low + (high - low)/2)
+
+    if ((mid == 0 or int_list[mid - 1] <= int_list[mid]) and (
+            mid == n - 1 or int_list[mid + 1] <= int_list[mid])):
+        return (int_list[mid])
+
+    elif (mid > 0 and int_list[mid - 1] > int_list[mid]):
+        return get_peak(int_list, low, (mid - 1), n)
+
+    else:
+        return get_peak(int_list, (mid + 1), high, n)
