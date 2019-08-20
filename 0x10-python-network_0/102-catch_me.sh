280d8f9bb4225489030a2f940e2cf6b5a85eadb8
klitscher <672@holbertonschool.com>
2019-08-20 16:46:39 -0400

    Hardcoding ip

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Grabs status code without a pipe
-curl -sL -X PUT "$1" -d "user_id=98" -H "Origin: HolbertonSchool"
+curl -sL -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
