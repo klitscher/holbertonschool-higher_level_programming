92ea7aa014990a46b158738720335842d971f536
klitscher <672@holbertonschool.com>
2019-08-20 14:13:37 -0400

    Adding files for exercie 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module to find the peak in a list of numbers"""
+
+
+def find_peak(list_of_integers):
+    """Function to find peak"""
+
+    if len(list_of_integers) < 1:
+        return ("None")
+    if len(list_of_integers) == 1:
+        return (list_of_integers[0])
+    base = list_of_integers[0]
+    for i in range(1, len(list_of_integers)):
+        if base < list_of_integers[i]:
+            base = list_of_integers[i]
+    return (base)
