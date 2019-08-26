b28acaa4cbbcba5cd88989fb09bd634910ad182e
klitscher <672@holbertonschool.com>
2019-08-26 18:46:06 +0000

    Update file for exercise 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Script to get a url and display the value of a header"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    req = requests.get(url)
+    print(req.headers.get('x-request-id'))
