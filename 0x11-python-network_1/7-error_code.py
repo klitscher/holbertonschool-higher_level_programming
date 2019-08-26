3749ffb91e857272ca740687570bcee779137ac1
klitscher <672@holbertonschool.com>
2019-08-26 19:10:03 +0000

    Update file for exercise 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Script to send a request and handle errors with request"""
+
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    req = requests.get(url)
+    if req.status_code >= 400:
+        print('Error code: {}'.format(req.status_code))
+    else:
+        print(req.text)
