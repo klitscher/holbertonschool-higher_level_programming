774995969f8d2dab90e53ae6a3976e54764f6fa6
klitscher <672@holbertonschool.com>
2019-08-26 18:37:45 +0000

    Updating file for exercise 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Script to get a url with requests"""
+
+import requests
+
+
+if __name__ == '__main__':
+    req = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t - type: {}'.format(type(req.text)))
+    print('\t - content: {}'.format(req.text))
