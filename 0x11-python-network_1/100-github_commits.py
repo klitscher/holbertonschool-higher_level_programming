882e0a4939ba12ed088901bb95f546b1bcf88d56
klitscher <672@holbertonschool.com>
2019-08-26 22:32:06 +0000

    Updating to slice instead of iterate through

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,10 +9,10 @@ if __name__ == '__main__':
     url = 'https://api.github.com/repos/{}/{}/commits'.format(
         sys.argv[2], sys.argv[1])
     req = requests.get(url)
-    result = req.json()
-    for i in range(10):
-        sha = result[i].get('sha')
-        commit = result[i].get('commit')
+    result = req.json()[:10]
+    for comm in result:
+        sha = comm.get('sha')
+        commit = comm.get('commit')
         author = commit.get('author')
         name = author.get('name')
         print("{}: {}".format(sha, name))
