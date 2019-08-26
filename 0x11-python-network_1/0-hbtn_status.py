2dc570918555805d07f6043333421c01301b0557
klitscher <672@holbertonschool.com>
2019-08-26 17:00:33 +0000

    Updating to decode correctly

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,6 +2,7 @@
 """Script to fetch a url"""
 import urllib.request
 
+
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
@@ -9,4 +10,4 @@ if __name__ == "__main__":
         print("Body response:")
         print("\t- type: {}".format(html_type))
         print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(response.msg))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
