32018cb11127a55f5fcfce87df027e6b90ccbb95
klitscher <672@holbertonschool.com>
2019-08-26 18:11:37 +0000

    Changing the encoding to utf-8

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     email = {'email': 'sys.argv[2]'}
 
     data = urllib.parse.urlencode(email)
-    data = data.encode('ascii')
+    data = data.encode('utf-8')
     req = urllib.request.Request(url, data)
 
     with urllib.request.urlopen(req) as response:
