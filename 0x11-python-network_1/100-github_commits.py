f5483b72f546192f2cc19c5a51664454b7d65c95
klitscher <672@holbertonschool.com>
2019-08-26 22:07:51 +0000

    Updating file for exercise 11

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Script to send a request to github api"""
+
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+        sys.argv[1], sys.argv[2])
+    req = requests.get(url)
+    result = req.json()
+    for i in range(10):
+        sha = result[i].get('sha')
+        commit = result[i].get('commit')
+        author = commit.get('author')
+        name = author.get('name')
+        print("{}: {}".format(sha, name))
