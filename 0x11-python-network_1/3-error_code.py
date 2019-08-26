161ad26d18d7679a1c49037b536f1ff8f03ff612
klitscher <672@holbertonschool.com>
2019-08-26 18:30:05 +0000

    Updating file for exercise 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Script to send a request and handle errors"""
+
+import sys
+import urllib.request
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+            print('Error code: {}'.format(e.code))
