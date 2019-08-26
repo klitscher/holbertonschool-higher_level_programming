182d2224fa760d0c2fd78a35843598c89a3d749a
klitscher <672@holbertonschool.com>
2019-08-26 18:41:51 +0000

    Updating to remove extra space

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,6 +6,7 @@ import requests
 
 if __name__ == '__main__':
     req = requests.get('https://intranet.hbtn.io/status')
+    req.encoding = 'utf-8'
     print('Body response:')
-    print('\t - type: {}'.format(type(req.text)))
-    print('\t - content: {}'.format(req.text))
+    print('\t- type: {}'.format(type(req.text)))
+    print('\t- content: {}'.format(req.text))
