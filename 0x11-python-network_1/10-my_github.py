08f3891faeacf3752c5499a32683e0b1b7e1a45d
klitscher <672@holbertonschool.com>
2019-08-26 21:10:36 +0000

    Update file for exercise 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Script to send a request to github api"""
+
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/user'
+    req = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
+    result = req.json()
+    print(result.get('id'))
