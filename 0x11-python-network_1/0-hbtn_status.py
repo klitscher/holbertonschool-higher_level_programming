930557ecc4233e62e89104385da019545dee9867
klitscher <672@holbertonschool.com>
2019-08-26 16:47:26 +0000

    Adding direcotry, readme, file for exercise 1 and dummy files for all other exercises

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Script to fetch a url"""
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        html_type = type(html)
+        print("Body response:")
+        print("\t- type: {}".format(html_type))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(response.msg))
