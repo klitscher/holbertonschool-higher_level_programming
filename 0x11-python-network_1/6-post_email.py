59159396e6546d52335b422bf2e6e504c1f1b8cc
klitscher <672@holbertonschool.com>
2019-08-26 18:59:40 +0000

    Update file for exercise 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Script to send a Post request with request"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = {'email': sys.argv[2]}
+    req = requests.post(url, data=email)
+    print(req.text)
