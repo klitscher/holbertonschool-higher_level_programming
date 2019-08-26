228b12daa41645913fd90f4dc1197fff75b1e38a
klitscher <672@holbertonschool.com>
2019-08-26 20:04:20 +0000

    Updating file for exercise 9

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Script to send a search request to a star wars api"""
+
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    search = {'search': sys.argv[1]}
+    url = 'https://swapi.co/api/people/'
+    req = requests.get(url, params=search)
+    json = req.json()
+    results = json.get('results')
+    print("Number of results: {}".format(json.get('count')))
+    for res in results:
+        print(res.get('name'))
