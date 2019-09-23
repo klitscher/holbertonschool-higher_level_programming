c398b10df1d3989aacd4cdeefa0176fcabbf1660
klitscher <672@holbertonschool.com>
2019-09-23 19:10:16 +0000

    Adding files for exercise 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+class Square extends require('./5-square') {
+  charPrint (c) {
+    const arr = [];
+    for (let i = this.width; i > 0; i--) {
+      if (c === undefined) {
+        arr.push('X');
+      } else {
+        arr.push(c);
+      }
+    }
+    for (let i = this.height; i > 0; i--) {
+      console.log(arr.join(''));
+    }
+  }
+}
+
+module.exports = Square;
