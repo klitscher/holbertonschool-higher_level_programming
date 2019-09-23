b56c07978a80ea021be8e11c195a5564204bcfd1
klitscher <672@holbertonschool.com>
2019-09-23 23:29:56 +0000

    Remvoing extra newline

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 const str1 = fs.readFileSync(process.argv[2], 'utf8') + '\n';
-const str2 = fs.readFileSync(process.argv[3], 'utf8') + '\n';
+const str2 = fs.readFileSync(process.argv[3], 'utf8');
 fs.appendFile(process.argv[4], str1, (err) => {
   if (err) throw err;
 });
