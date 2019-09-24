2d587f6d85046e3d5e492b7bb33921cf414c00a8
klitscher <672@holbertonschool.com>
2019-09-24 17:51:47 +0000

    Updating to be less explicit

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,9 +5,9 @@ request(url, function (error, response, body) {
   if (error) { return; }
   let count = 0;
   const results = JSON.parse(body).results;
-  for (const films of results) {
-    for (const chara of films.characters) {
-      if (chara === 'https://swapi.co/api/people/18/') {
+  for (const film of results) {
+    for (const chara of film.characters) {
+      if (chara.includes('/18') || chara.includes('/18/')) {
         count++;
       }
     }
