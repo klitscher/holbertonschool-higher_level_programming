fbe97b4a006a6960cdd475dd7f74340d3f0e5083
klitscher <672@holbertonschool.com>
2019-09-24 02:16:40 -0400

    Adding file for exercise 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) {}
+  console.log(JSON.parse(body).title);
+});
