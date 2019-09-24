1a1e32724e86d8a0dfc1a992fe9b47e9b3fcb4ca
klitscher <672@holbertonschool.com>
2019-09-24 17:28:02 +0000

    Updating file to return on error

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,6 +3,6 @@ const request = require('request');
 let url = process.argv[2];
 url = 'http://swapi.co/api/people/18';
 request(url, function (error, response, body) {
-  if (error) {}
+  if (error) { return; }
   console.log(JSON.parse(body).films.length);
 });
