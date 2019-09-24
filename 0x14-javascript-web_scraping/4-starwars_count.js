0802459bf50f0cadfcd0e637f9a6d1e338d99bf9
klitscher <672@holbertonschool.com>
2019-09-24 02:27:53 -0400

    Added file for exercise 4 Hopefully they let me change the endpoint, as theirs was useless :)

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const request = require('request');
+let url = process.argv[2];
+url = 'http://swapi.co/api/people/18'
+request(url, function (error, response, body) {
+  if (error) {}
+  console.log(JSON.parse(body).films.length);
+});
